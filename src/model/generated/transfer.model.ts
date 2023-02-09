4322741871915d7c2e788027b89a3fbb4927e093
misterjame <103995267+misterjame@users.noreply.github.com>
2023-02-09 17:55:18 +0700

    Scale dedicated

--- src/model/generated/transfer.model.ts
+++ src/model/generated/transfer.model.ts
@@ -5,34 +5,34 @@ import {Owner} from "./owner.model"
 
 @Entity_()
 export class Transfer {
-  constructor(props?: Partial<Transfer>) {
-    Object.assign(this, props)
-  }
+    constructor(props?: Partial<Transfer>) {
+        Object.assign(this, props)
+    }
 
-  @PrimaryColumn_()
-  id!: string
+    @PrimaryColumn_()
+    id!: string
 
-  @Index_()
-  @ManyToOne_(() => Token, {nullable: true})
-  token!: Token
+    @Index_()
+    @ManyToOne_(() => Token, {nullable: true})
+    token!: Token
 
-  @Index_()
-  @ManyToOne_(() => Owner, {nullable: true})
-  from!: Owner | undefined | null
+    @Index_()
+    @ManyToOne_(() => Owner, {nullable: true})
+    from!: Owner | undefined | null
 
-  @Index_()
-  @ManyToOne_(() => Owner, {nullable: true})
-  to!: Owner | undefined | null
+    @Index_()
+    @ManyToOne_(() => Owner, {nullable: true})
+    to!: Owner | undefined | null
 
-  @Index_()
-  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
-  timestamp!: bigint
+    @Index_()
+    @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
+    timestamp!: bigint
 
-  @Index_()
-  @Column_("int4", {nullable: false})
-  block!: number
+    @Index_()
+    @Column_("int4", {nullable: false})
+    block!: number
 
-  @Index_()
-  @Column_("text", {nullable: false})
-  transactionHash!: string
+    @Index_()
+    @Column_("text", {nullable: false})
+    transactionHash!: string
 }
